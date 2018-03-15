2dff314d5fd7a89fbbe999c37bfe183c1b0d060d
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-03-15 14:48:54 -0400

    A very simple custom directive example. Initial Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { InputFormatDirective } from './input-format.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormatDirective
   ],
   imports: [
     BrowserModule
